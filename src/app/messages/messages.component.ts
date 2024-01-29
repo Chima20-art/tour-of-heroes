796df5431837075f0b543376eae6eb60e1bb017c
chaimae <chaimae.michich@fiscalteam.be>
2024-01-29 16:36:54 +0100

    removed unused imports

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,7 +1,6 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 import { MessageService} from "../message.service";
 import {NgForOf, NgIf} from "@angular/common";
-import {Hero} from "../hero";
 
 @Component({
   selector: 'app-messages',
